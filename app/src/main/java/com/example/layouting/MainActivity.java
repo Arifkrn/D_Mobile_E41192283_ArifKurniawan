6b3f7b51ca1151b9f744c5ca9589033e02ba66af
Arifkrn <71825520+Arifkrn@users.noreply.github.com>
2021-03-28 21:11:40 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/layouting/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.layouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void actionLinLayout(View view) {
+        Toast.makeText(this, "anda klik 1", Toast.LENGTH_SHORT).show();
+    }
+}
